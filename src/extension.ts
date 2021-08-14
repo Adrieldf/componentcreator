1ea7d7c7d3573e8abfab8e526ec90d8eb662ae0b
Adriel Izoton de Faci <adriel.faci@promob.com>
2021-08-13 23:04:53 -0300

    getting folders

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
+import { LocalStorageService } from "./localStorageService";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
@@ -9,44 +10,74 @@ export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand(
     "componentcreator.createComponent",
-      async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-          vscode.window.showInformationMessage("editor does not exist");
-          return;
-        }
-		console.log("entrou no comando");
-		var workspaceFolders = vscode.workspace.workspaceFolders;
-		if(workspaceFolders != null && workspaceFolders.length > 0){
-			const projectRoot = workspaceFolders[0].uri.fsPath;
-      
-			
-		}
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("editor does not exist");
+        return;
+      }
 
-
-		var baseComponentName = "";
-        //mostrar quickpick com a lista de componentes e selecionar um
-        const quickPick = vscode.window.createQuickPick();
-        var folders = vscode.workspace.workspaceFolders;
-		
-		var items = folders?.map((x) => ({ label: x.name }));
-		
-		console.log("itens: ", items);
-     //  quickPick.items = items;
-        quickPick.onDidChangeSelection(([item]) => {
-          if (item) {
-			  baseComponentName = item.label;
-            quickPick.dispose();
+      function lookForFolders(
+        currentPath: string,
+        folderList: string[],
+        rootFolderName: string
+      ) {
+        let read = fs.readdirSync(currentPath, { withFileTypes: true });
+        read.forEach((element) => {
+          if (element.isDirectory() && element.name != "node_modules") {
+            folderList.push(
+              currentPath
+                .substring(currentPath.lastIndexOf(rootFolderName))
+                .concat("\\", element.name)
+            );
+            lookForFolders(
+              currentPath.concat("\\", element.name),
+              folderList,
+              rootFolderName
+            );
           }
         });
-		quickPick.onDidHide(() => quickPick.dispose());
-		quickPick.show();
-        //digitar o nome do componente a ser criado
-        //copiar a pasta do componente selecionado e criar nova com o nome escrito
-        //copiar os arquivos de dentro do componente base e criar novos substituindo o nome do antigo pelo novo aonde estiver no titulo e conteudo do arquivo
-
-        
       }
+
+      function saveComponentFolder() {
+        let storageManager = new LocalStorageService(context.workspaceState);
+
+        storageManager.setValue<string>("componentsFolder", "");
+
+        let componentsFolder = storageManager.getValue<string>("componentsFolder");
+
+        if(componentsFolder && componentsFolder != "" ){
+          
+        }
+        var workspaceFolders = vscode.workspace.workspaceFolders;
+        let folderList = Array<string>();
+        if (workspaceFolders != null && workspaceFolders.length > 0) {
+          const projectRoot = workspaceFolders[0].uri.fsPath;
+          lookForFolders(
+            projectRoot,
+            folderList,
+            projectRoot.substring(projectRoot.lastIndexOf("\\"))
+          );
+        }
+        var baseComponentName = "";
+        const quickPick = vscode.window.createQuickPick();
+        if (folderList.length) {
+          quickPick.items = folderList.map((label) => ({ label }));
+          quickPick.onDidChangeSelection(([item]) => {
+            if (item) {
+              baseComponentName = item.label;
+              quickPick.dispose();
+            }
+          });
+          quickPick.onDidHide(() => quickPick.dispose());
+          quickPick.show();
+        }
+      }
+
+      //digitar o nome do componente a ser criado
+      //copiar a pasta do componente selecionado e criar nova com o nome escrito
+      //copiar os arquivos de dentro do componente base e criar novos substituindo o nome do antigo pelo novo aonde estiver no titulo e conteudo do arquivo
+    }
   );
 
   context.subscriptions.push(disposable);
